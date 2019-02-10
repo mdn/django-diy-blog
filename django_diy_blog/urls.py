c06cb7887505b790a8696a1bb0a407b013e5468c
Hamish Willee <hamishwillee@gmail.com>
2019-02-10 15:04:21 +1100

    Minimal changes to update to Django 2.1.5

--- django_diy_blog/urls.py
+++ django_diy_blog/urls.py
@@ -1,7 +1,7 @@
 """django_diy_blog URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.0/topics/http/urls/
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,7 +13,6 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-
 from django.contrib import admin
 from django.urls import path
 
