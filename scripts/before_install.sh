699be48748addb5b4668a718fc091579fc6971e9
Oğuzhan Çelikarslan <oguzhancelikarslan@gmail.com>
2021-05-23 20:10:02 +0300

    updates before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+sudo yum update -y
+
+sudo yum install git -y
+sudo yum install python3-pip python3-devel python3-setuptools -y
+
 DIR="/home/ubuntu/app"
 if [ -d "$DIR" ]; then
   echo "${DIR} exists"
@@ -10,6 +15,7 @@ fi
 
 sudo chmod -R 777 /home/ubuntu/app
 cd /home/ubuntu/app
+
 
 pip3 install -r requirements.txt
 python3 manage.py migrate
